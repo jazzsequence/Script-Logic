86769870b188e5a0793f58fe6b815541e7122d5f
chris reynolds <chris@jazzsequence.com>
2012-06-07 15:16:14 -0600

    adds submenu page

--- script-logic.php
+++ script-logic.php
@@ -9,3 +9,20 @@ Author URI: http://museumthemes.com
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl.html
 */
+
+/**
+ * add page
+ * @author Chris Reynolds
+ * @since 0.1
+ * @uses add_submenu_page
+ * @uses add_action
+ * @uses ap_script_logic_page
+ * @uses ap_script_logic_scripts
+ * this adds the submenu page under Tools
+ */
+function ap_script_logic_add_page() {
+	$page = add_submenu_page('tools.php', 'Script Logic', 'Script Logic', 'administrator', 'ap_script_logic_page', 'ap_script_logic_page');
+	add_action('admin_print_scripts-' . $page, 'ap_script_logic_scripts');
+}
+
+?>
